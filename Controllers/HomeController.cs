27acea1a6c6b008beb9a864b41f9c91cdfcac61e
Nataliya Zhuravleva <natalindria@gmail.com>
2021-01-08 15:20:58 -0800

    Add lists to homecontroller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,27 @@
+using Factory.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace Factory.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
     {
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
+      _db = db;
     }
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      List<Engineer> engineers = _db.Engineers.ToList();
+      List<Machine> machines = _db.Machines.ToList();
+      List<List<EngineerMachine>> model = new List<List<EngineerMachine>>(engineers, machines);
+      return View(model);
+    }
+  }
 }
