221e3db212d32e6aa6650d9d14d2a366b0aee80f
Nataliya Zhuravleva <natalindria@gmail.com>
2021-01-08 17:11:25 -0800

    Add nataliya_zhuravleva.sql

---
+++ nataliya_zhuravleva.sql
@@ -0,0 +1,135 @@
+-- MySQL dump 10.13  Distrib 8.0.15, for macos10.14 (x86_64)
+--
+-- Host: localhost    Database: nataliya_zhuravleva
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `__EFMigrationsHistory`
+--
+
+DROP TABLE IF EXISTS `__EFMigrationsHistory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `__EFMigrationsHistory` (
+  `MigrationId` varchar(95) NOT NULL,
+  `ProductVersion` varchar(32) NOT NULL,
+  PRIMARY KEY (`MigrationId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `__EFMigrationsHistory`
+--
+
+LOCK TABLES `__EFMigrationsHistory` WRITE;
+/*!40000 ALTER TABLE `__EFMigrationsHistory` DISABLE KEYS */;
+INSERT INTO `__EFMigrationsHistory` VALUES ('20210108184403_Initial','2.2.4-servicing-10062'),('20210108200842_ChangeEngineer','2.2.4-servicing-10062');
+/*!40000 ALTER TABLE `__EFMigrationsHistory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `EngineerMachine`
+--
+
+DROP TABLE IF EXISTS `EngineerMachine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `EngineerMachine` (
+  `EngineerMachineId` int(11) NOT NULL AUTO_INCREMENT,
+  `EngineerId` int(11) NOT NULL,
+  `MachineId` int(11) NOT NULL,
+  PRIMARY KEY (`EngineerMachineId`),
+  KEY `IX_EngineerMachine_EngineerId` (`EngineerId`),
+  KEY `IX_EngineerMachine_MachineId` (`MachineId`),
+  CONSTRAINT `FK_EngineerMachine_Engineers_EngineerId` FOREIGN KEY (`EngineerId`) REFERENCES `engineers` (`EngineerId`) ON DELETE CASCADE,
+  CONSTRAINT `FK_EngineerMachine_Machines_MachineId` FOREIGN KEY (`MachineId`) REFERENCES `machines` (`MachineId`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `EngineerMachine`
+--
+
+LOCK TABLES `EngineerMachine` WRITE;
+/*!40000 ALTER TABLE `EngineerMachine` DISABLE KEYS */;
+INSERT INTO `EngineerMachine` VALUES (6,8,5),(7,9,5),(8,7,4),(9,9,3);
+/*!40000 ALTER TABLE `EngineerMachine` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Engineers`
+--
+
+DROP TABLE IF EXISTS `Engineers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `Engineers` (
+  `EngineerId` int(11) NOT NULL AUTO_INCREMENT,
+  `EngineerFirstName` longtext,
+  `EngineerLastName` longtext,
+  `EngineerLicenseNumber` longtext,
+  `EngineerDateOfLicenseRenewal` datetime(6) NOT NULL DEFAULT '0001-01-01 00:00:00.000000',
+  PRIMARY KEY (`EngineerId`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Engineers`
+--
+
+LOCK TABLES `Engineers` WRITE;
+/*!40000 ALTER TABLE `Engineers` DISABLE KEYS */;
+INSERT INTO `Engineers` VALUES (7,'Ariana','Grande','RL 12345','2021-05-01 00:00:00.000000'),(8,'Justin','Bieber','RL 54321','2021-02-01 00:00:00.000000'),(9,'Lady','Gaga','RL 56789','2020-03-03 00:00:00.000000');
+/*!40000 ALTER TABLE `Engineers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Machines`
+--
+
+DROP TABLE IF EXISTS `Machines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `Machines` (
+  `MachineId` int(11) NOT NULL AUTO_INCREMENT,
+  `MachineBrandName` longtext,
+  `MachineModelName` longtext,
+  `MachineProductionYear` longtext,
+  `MachineSerialNumber` longtext,
+  `DateOfMachineInspection` datetime(6) NOT NULL,
+  PRIMARY KEY (`MachineId`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Machines`
+--
+
+LOCK TABLES `Machines` WRITE;
+/*!40000 ALTER TABLE `Machines` DISABLE KEYS */;
+INSERT INTO `Machines` VALUES (3,'VW','Jetta','2020','BYGD4532','2021-01-11 00:00:00.000000'),(4,'Toyota','Corolla','2018','SDFF4983','2020-12-23 00:00:00.000000'),(5,'BMW','X5','2015','DFGH4400','2020-11-14 00:00:00.000000');
+/*!40000 ALTER TABLE `Machines` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-08 17:07:26
